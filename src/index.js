49a0574bb9700bdf4c31bfe72dcb9a963068ff4c
Sergii Bomko <1164492+aquiladev@users.noreply.github.com>
2021-12-14 15:12:50 +0530

    add ga

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactGA from 'react-ga';
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+ReactGA.initialize('UA-215305995-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(
   <React.StrictMode>
